006f1f3f0527924a104e71bdf97d0185153371fe
Shivanshu Kumar <shivanshu19476@iiitd.ac.in>
2026-06-15 13:49:52 +0530

    Add PDF parsing module

---
+++ modules/parser.py
@@ -0,0 +1,18 @@
+# modules/parser.py
+
+import pdfplumber
+
+def extract_pdf_text(pdf_path):
+
+    text = ""
+
+    with pdfplumber.open(pdf_path) as pdf:
+
+        for page in pdf.pages:
+
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text
+
+    return text
